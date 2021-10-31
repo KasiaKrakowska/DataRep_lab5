ef6d990a28a7b50af9f731704e8990397b2f7256
Kasia Krakowska <G00364903@gmit.ie>
2021-10-31 12:07:49 +0000

    Lab_5 first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+//a module with functions or objects or variables assigned to it
+const express = require('express')
+//function is used to mount the specified middleware function(s) at the path which is being specified
+const app = express()
+//This app starts a server and listens on port 3000 for connections
+const port = 3000
+
+//function define a route handler for GET requests to a given URL
+app.get('/', (req, res) => {
+    //This function accepts a single parameter body that describe the body
+    // which is to be sent in the response
+    res.send('Welcome to Data Representation & Querying.')
+})
+
+//used to bind and listen the connections on the specified host and port
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
