7d4ce34082e07677e5dcc1656757865f549fa3ab
Kasia Krakowska <G00364903@gmit.ie>
2021-10-31 12:31:10 +0000

    Lab_5 third commit

--- server.js
+++ server.js
@@ -14,11 +14,35 @@ app.get('/', (req, res) => {
 })
 
 //function define a route handler for GET requests to a given URL
-app.get('/hello/:name', (req, res)=>{
+app.get('/hello/:name', (req, res) => {
     //identifies name request
     console.log(req.params.name)
     //sends back Hello on different URL
     res.send('Hello ' + req.params.name);
+})
+
+//function define a route handler for GET requests to a given URL
+app.get('/api/movies', (req, res) => {
+    //creates variable with movies json data
+    const mymovies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        }
+    ];
+
+    //This function sends JSON data in the response 
+    res.json({movies: mymovies});
 })
 
 //used to bind and listen the connections on the specified host and port
