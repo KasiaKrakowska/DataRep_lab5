b29d8be23169f499f74b47858ac594739e4cc484
Kasia Krakowska <G00364903@gmit.ie>
2021-10-31 12:21:08 +0000

    Lab_5 second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //a module with functions or objects or variables assigned to it
 const express = require('express')
-//function is used to mount the specified middleware function(s) at the path which is being specified
+//function is used to mount the specified middleware function(s) at the path
+//which is being specified
 const app = express()
 //This app starts a server and listens on port 3000 for connections
 const port = 3000
@@ -10,6 +11,14 @@ app.get('/', (req, res) => {
     //This function accepts a single parameter body that describe the body
     // which is to be sent in the response
     res.send('Welcome to Data Representation & Querying.')
+})
+
+//function define a route handler for GET requests to a given URL
+app.get('/hello/:name', (req, res)=>{
+    //identifies name request
+    console.log(req.params.name)
+    //sends back Hello on different URL
+    res.send('Hello ' + req.params.name);
 })
 
 //used to bind and listen the connections on the specified host and port
